991eff0ba230db15c1156c00a2cdd087875c8e64
Nurbu <lamanurbu2@gmail.com>
2026-04-16 12:31:52 -0400

    Added the thread sleep to slow down the count between every second.

--- src/main/java/com/pluralsight/ForLoop.java
+++ src/main/java/com/pluralsight/ForLoop.java
@@ -1,14 +1,15 @@
 package com.pluralsight;
 
 public class ForLoop {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         // Prints count down from 10 to 1 and then prints launch
-
+        Thread.sleep(1000);
         for (int i = 10; i > -1; i--) {
             if (i == 0) {
                 System.out.println("Launch!");
                 continue;
             }
+
             System.out.println("Launch in " + i);
 
         }
