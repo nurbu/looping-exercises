3f8b5f68a8fdcbf2f53327150e244794550d2da3
Nurbu <lamanurbu2@gmail.com>
2026-04-16 13:11:14 -0400

    Moved the Thread inside the loop.

--- src/main/java/com/pluralsight/ForLoop.java
+++ src/main/java/com/pluralsight/ForLoop.java
@@ -3,15 +3,14 @@ package com.pluralsight;
 public class ForLoop {
     public static void main(String[] args) throws InterruptedException {
         // Prints count down from 10 to 1 and then prints launch
-        Thread.sleep(1000);
+
         for (int i = 10; i > -1; i--) {
+            Thread.sleep(1000);
             if (i == 0) {
                 System.out.println("Launch!");
                 continue;
             }
-
             System.out.println("Launch in " + i);
-
         }
     }
 }
