d49513b7a882d976a025b86971899fb21c67c113
Nurbu <lamanurbu2@gmail.com>
2026-04-16 12:11:01 -0400

    WhileLoop class completed.

---
+++ src/main/java/com/pluralsight/WhileLoop.java
@@ -0,0 +1,13 @@
+package com.pluralsight;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+        // Prints "I love Java" 5 times
+
+        int i = 0;
+        while (i < 5) {
+            System.out.println("I love Java");
+            i++;
+        }
+    }
+}
